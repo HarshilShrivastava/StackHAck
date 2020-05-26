bd02e3ccee5d929587db1707466451b1e74b45d0
HarshilShrivastava <Shrivastavahharshil12@gmail.com>
2020-05-26 13:53:21 +0530

    static files

--- StackHack/StackHack/settings.py
+++ StackHack/StackHack/settings.py
@@ -129,3 +129,6 @@ CORS_ORIGIN_ALLOW_ALL=True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
